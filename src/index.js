cd955d3fee9bfd598a37b89e5f8e80e8782a7713
anastan588 <anastasiya5.88@mail.ru>
2022-07-18 21:40:22 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let number = Math.abs(n);
+    let numberForArray = number.toString().split('');
+    let reverseArray = numberForArray.reverse();
+    let reverseString = reverseArray.join('');
+    let reverseNumber = Number(reverseString);
+    return reverseNumber;
 }
